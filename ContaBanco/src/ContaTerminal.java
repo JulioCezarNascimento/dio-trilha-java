d6e8ca0da30c5d4d7dbad5a104ec3482674221ff
JulioCezarNascimento <silvajuliocezar992@gmail.com>
2023-12-15 17:28:22 -0300

    Criada as primeiras classes.

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        
+        //TODO:Conhecer e importar a classe Scanner
+        System.out.println("Por favor, informe o número da sua Agencia:");
+        String agencia = sc.nextLine();
+        //Exibir as mensagens para o nosso cliente
+        System.out.println("Por favor, informe o número da sua Conta:");
+        int numero = sc.nextInt();
+        sc.nextLine(); //Para consumir a quebra de linha
+        //Obter pelo scaneer os valores digitados no terminal
+        
+        //Exibir a mensagem conta criada
+    
     }
 }
