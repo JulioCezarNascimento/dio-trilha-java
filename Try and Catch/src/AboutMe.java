90bc32bad881d35b003bc782001bc01c8bce29c2
JulioCezarNascimento <silvajuliocezar992@gmail.com>
2024-01-03 15:21:40 -0300

    Pasta da aula Try and Catch finalizada.

--- Try and Catch/src/AboutMe.java
+++ Try and Catch/src/AboutMe.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -5,20 +6,26 @@ public class AboutMe {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Qual seu nome?");
-        String nome = sc.next();
+        try{
+            System.out.println("Qual seu nome?");
+            String nome = sc.next();
 
-        System.out.println("Qual seu sobrenome?");
-        String sobrenome = sc.next();
+            System.out.println("Qual seu sobrenome?");
+            String sobrenome = sc.next();
 
-        System.out.println("Digite sua idade");
-        int idade = sc.nextInt();
+            System.out.println("Digite sua idade");
+            int idade = sc.nextInt();
 
-        System.out.println("Digite sua altura");
-        double altura = sc.nextDouble();
+            System.out.println("Digite sua altura");
+            double altura = sc.nextDouble();
 
-        //Imprimindo os dados obtidos pelo usuario
-        System.out.println("Ola, me chamo" + nome.toUpperCase() + " " + sobrenome.toLowerCase());
-        System.out.println("Tenho" + idade + "anos, e minha altura e" + altura + "metros");
+            //Imprimindo os dados obtidos pelo usuario
+            System.out.println("Ola, me chamo" + nome.toUpperCase() + " " + sobrenome.toLowerCase());
+            System.out.println("Tenho" + idade + "anos");
+            System.out.println("Minha altura e" + altura + "m");
+            sc.close();
+        }catch(InputMismatchException e){
+            System.err.println("Os campos idade e altura devem ser numericos");
+        }
     }
 }
